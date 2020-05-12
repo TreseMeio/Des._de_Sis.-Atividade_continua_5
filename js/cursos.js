f99d2849aff3582a85d54fad507202ca3acc6056
Leonardo Natario Farias <leonatifarias@gmail.com>
2020-05-11 23:43:05 -0300

    adicionando as descricoes finais

--- js/cursos.js
+++ js/cursos.js
@@ -1,8 +1,10 @@
 var slideIndex = 0;
+const descricoes = ["Um curso voltado a desenvolvimento de soluções totalmente moderno e inovador. Nosso foco é a linguagem Python. Neste curso você aprenderá as melhores práticas de desenvolvimento e operação necessários para criar soluções de sucesso", "Um curso voltado totalmente à análise e a construir valor a partir de informação fora de conjunto. Neste curso você aprenderá a perceber padrões, verificar possibilidades, prever e descrever as melhores oportunidades para o negócio", "ble" ,"bli"]
 showSlides();
 
 function showSlides() {
   var i;
+  var description = document.getElementById('descricao');
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
@@ -15,5 +17,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  description.innerHTML = descricoes[slideIndex - 1];
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
