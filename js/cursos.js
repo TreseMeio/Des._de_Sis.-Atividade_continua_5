fac284faa5a2eb22c113e22968aeb0ef279b6205
Leonardo Natario Farias <leonatifarias@gmail.com>
2020-05-11 23:51:47 -0300

    navegando para as páginas internas de cursos

--- js/cursos.js
+++ js/cursos.js
@@ -1,22 +1,44 @@
 var slideIndex = 0;
-const descricoes = ["Um curso voltado a desenvolvimento de soluções totalmente moderno e inovador. Nosso foco é a linguagem Python. Neste curso você aprenderá as melhores práticas de desenvolvimento e operação necessários para criar soluções de sucesso", "Um curso voltado totalmente à análise e a construir valor a partir de informação fora de conjunto. Neste curso você aprenderá a perceber padrões, verificar possibilidades, prever e descrever as melhores oportunidades para o negócio", "ble" ,"bli"]
+const descricoes = [
+    {
+        action: "ADS",
+        description: "Um curso voltado a desenvolvimento de soluções totalmente moderno e inovador. Nosso foco é a linguagem Python. Neste curso você aprenderá as melhores práticas de desenvolvimento e operação necessários para criar soluções de sucesso"
+    },
+    {
+        action: "BD",
+        description: "Um curso voltado totalmente à análise e a construir valor a partir de informação fora de conjunto. Neste curso você aprenderá a perceber padrões, verificar possibilidades, prever e descrever as melhores oportunidades para o negócio"
+    },
+    {
+        action: "GTI",
+        description: "ble"
+    },
+    {
+        action: "SI",
+        description: "bli"
+    }
+];
 showSlides();
 
 function showSlides() {
-  var i;
-  var description = document.getElementById('descricao');
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  description.innerHTML = descricoes[slideIndex - 1];
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+    var i;
+    var navegarCurso = document.getElementById('navegarCurso');
+    var description = document.getElementById('descricao');
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    description.innerHTML = descricoes[slideIndex - 1].description;
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+function consultar() {
+    window.location.href = `${descricoes[slideIndex - 1].action}.html`;
 }
